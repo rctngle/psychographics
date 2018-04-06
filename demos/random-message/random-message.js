5cd3159236db0400879a679404a61a5d6563972e
Elisabeth Malcolm <malcolm.elisabeth@googlemail.com>
2018-04-06 13:59:29 +0100

    added login sessions heatmap

---
+++ demos/random-message/random-message.js
@@ -0,0 +1,28 @@
+let messages;
+
+fetch('../../facebook/messages.json', {
+	method: 'get'
+}).then((response) => {
+	return response.json();
+}).then((data) => {
+	messages = data;
+	console.log(messages);
+	showRandomMessage();
+	setInterval(showRandomMessage, 2000);
+});
+
+
+
+function showRandomMessage(){
+	const idx = Math.floor(Math.random() * messages.length);
+	const message = messages[idx];
+	const cidx = Math.floor(Math.random() * message.conversation.length);
+	if(message.conversation.length === 0){
+		showRandomMessage();
+	} else {
+		const midx = Math.floor(Math.random() * message.conversation[cidx].messages.length);
+		if(message.conversation[cidx].messages.length !== 0){
+			document.querySelector('#message').textContent = message.conversation[cidx].messages[midx].text;	
+		}		
+	}	
+}
