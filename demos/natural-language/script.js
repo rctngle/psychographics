a6c452238e42dac31ba5e001c575ed386e24bdfa
Lizzie Malcolm <malcolm.elisabeth@googlemail.com>
2018-04-08 19:19:11 +0200

    added filter to sentiment analysis

--- demos/natural-language/script.js
+++ demos/natural-language/script.js
@@ -3,14 +3,20 @@ fetch('sentences.json', {
 }).then((response) => {
 	return response.json();
 }).then((data) => {
-	data.forEach((sentence) => {
-
+	data.forEach((sentence, i) => {		
 		const li = document.createElement('li');
+	
 		li.innerHTML = `
 			<span class="score">${sentence.sentiment}</span>
 			<span class="sentence">${sentence.text}</span>
 		`;
 
 		document.querySelector('#sentences').appendChild(li);
+
 	});
+	var options = {
+	    valueNames: [ 'score', 'sentence' ]
+	};
+	var list = new List('sentences-container', options);
+
 });
